1ec27084885424eecee9b3dcbbdbc3cfeb43cf6f
FelixSFD <felix.deil@student.uni-augsburg.de>
2017-01-25 19:43:55 +0100

    #5: + added different text, when matched posts have the same author

--- src/main/java/org/sobotics/guttenberg/printers/SoBoticsPostPrinter.java
+++ src/main/java/org/sobotics/guttenberg/printers/SoBoticsPostPrinter.java
@@ -15,11 +15,23 @@ public class SoBoticsPostPrinter implements PostPrinter {
     public String print(PlagFinder finder) {
     	
     	double score = Math.round(finder.getJaroScore()*100.0)/100.0;
-    	String link = finder.getJaroAnswer().get("link").getAsString();
+    	String link = "https://stackoverflow.com/a/"+finder.getJaroAnswer().get("answer_id").getAsString();
     	String targetLink = "https://stackoverflow.com/a/"+finder.getTargetAnswer().get("answer_id").getAsString();
     	
-    	String post = "[ [Guttenberg](https://git.io/vMrPa) ] [Possible plagiarism]("+targetLink+") with a score of **"+ score +"**. [Original post]("+link+")";
+    	int userOne = finder.getJaroAnswer().get("owner").getAsJsonObject().get("user_id").getAsInt();
+    	int userTwo = finder.getTargetAnswer().get("owner").getAsJsonObject().get("user_id").getAsInt();
     	
+    	String post;
+    	
+    	if (userOne != userTwo) {
+    		//plagiarism; different users
+    		post = "[ [Guttenberg](https://git.io/vMrPa) ] [Possible plagiarism]("+targetLink+") with a score of **"+ score +"**. [Original post]("+link+")";
+    	} else {
+    		//duplicated answer; same user
+    		String user = finder.getTargetAnswer().get("owner").getAsJsonObject().get("display_name").getAsString();
+    		post = "[ [Guttenberg](https://git.io/vMrPa) ] User ["+user+"](http://stackoverflow.com/users/"+userTwo+") posted [this answer]("+targetLink+") after his [original answer]("+link+"). Score: **"+ score +"**";
+    	}
+    	    	
         return post;
     }
 }
