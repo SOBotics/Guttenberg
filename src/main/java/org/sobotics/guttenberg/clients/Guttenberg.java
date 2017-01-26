9b42ec3cfe846318b754a905e7a74ba8296afe0c
FelixSFD <felix.deil@student.uni-augsburg.de>
2017-01-26 15:43:14 +0100

    - put scheduled executions in separate threads

--- src/main/java/org/sobotics/guttenberg/clients/Guttenberg.java
+++ src/main/java/org/sobotics/guttenberg/clients/Guttenberg.java
@@ -46,11 +46,15 @@ public class Guttenberg {
     private List<BotRoom> rooms;
     private List<Room> chatRooms;
     private ScheduledExecutorService executorService;
+    private ScheduledExecutorService executorServiceCheck;
+    private ScheduledExecutorService executorServiceUpdate;
 	
 	public Guttenberg(StackExchangeClient client, List<BotRoom> rooms) {
 		this.client = client;
 		this.rooms = rooms;
 		this.executorService = Executors.newSingleThreadScheduledExecutor();
+		this.executorServiceCheck = Executors.newSingleThreadScheduledExecutor();
+		this.executorServiceUpdate = Executors.newSingleThreadScheduledExecutor();
 		chatRooms = new ArrayList<>();
 		
 		this.setLogfile();
@@ -87,8 +91,8 @@ public class Guttenberg {
 		
 		
 		executorService.scheduleAtFixedRate(()->execute(), 15, 59, TimeUnit.SECONDS);
-		executorService.scheduleAtFixedRate(()->checkLastExecution(), 3, 5, TimeUnit.MINUTES);
-		executorService.scheduleAtFixedRate(()->update(), 0, 30, TimeUnit.MINUTES);
+		executorServiceCheck.scheduleAtFixedRate(()->checkLastExecution(), 3, 5, TimeUnit.MINUTES);
+		executorServiceUpdate.scheduleAtFixedRate(()->update(), 0, 30, TimeUnit.MINUTES);
 	}
 	
 	private void execute() {
