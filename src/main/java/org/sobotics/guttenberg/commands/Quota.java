06571c9bfc3fd696d03365f6ab000a1979c6ace6
FelixSFD <felix.deil@student.uni-augsburg.de>
2017-03-13 14:51:37 +0100

    #75: - made the quota command available in standby mode

--- src/main/java/org/sobotics/guttenberg/commands/Quota.java
+++ src/main/java/org/sobotics/guttenberg/commands/Quota.java
@@ -1,7 +1,15 @@
 package org.sobotics.guttenberg.commands;
 
 import org.sobotics.guttenberg.utils.CommandUtils;
+import org.sobotics.guttenberg.utils.FilePathUtils;
 import org.sobotics.guttenberg.utils.StatusUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sobotics.guttenberg.services.RunnerService;
 
 import fr.tunaki.stackoverflow.chat.Message;
@@ -9,6 +17,7 @@ import fr.tunaki.stackoverflow.chat.Room;
 
 public class Quota implements SpecialCommand {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Quota.class);
     private final Message message;
 
     public Quota(Message message) {
@@ -22,7 +31,17 @@ public class Quota implements SpecialCommand {
 
     @Override
     public void execute(Room room, RunnerService instance) {
-        room.replyTo(this.message.getId(), "The remaining quota is: "+StatusUtils.remainingQuota);
+    	Properties prop = new Properties();
+
+        try{
+            prop.load(new FileInputStream(FilePathUtils.loginPropertiesFile));
+        }
+        catch (IOException e){
+            LOGGER.error("Error: ", e);
+            return;
+        }
+        
+        room.send("The remaining quota on "+prop.getProperty("location", "undefined")+" is: "+StatusUtils.remainingQuota);
     }
 
     @Override
